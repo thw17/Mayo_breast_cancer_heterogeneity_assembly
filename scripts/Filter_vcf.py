6e994b1ec6c4ce4498bd816355f3d312df92382e
thw17 <twebster17@gmail.com>
2017-07-26 01:39:03 -0700

    troubleshooting

--- scripts/Filter_vcf.py
+++ scripts/Filter_vcf.py
@@ -103,7 +103,7 @@ def main():
 					continue
 		dp = variant.format('DP')
 		dp = dp[np.where(dp >= args.sample_depth)]
-		print(dp)
+		print(len(dp))
 		if len(dp) < args.min_samples:
 			continue
 		gq = variant.format('GQ')
