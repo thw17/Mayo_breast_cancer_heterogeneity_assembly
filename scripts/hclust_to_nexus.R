e358635bb5c67abab587b7907338c92826274bb0
thw17 <twebster17@gmail.com>
2018-09-03 21:16:06 -0700

    troubleshooting

--- scripts/hclust_to_nexus.R
+++ scripts/hclust_to_nexus.R
@@ -19,6 +19,7 @@ phylo_list <- vector("list", length(input_files))
 
 i <- 1
 while(i < length(input_files)) {
+	print(input_files[i])
 	input_table <- input_files[i][, -1]
 	rownames(input_table) <- sample_names
 	dist_matrix <- dist(input_table, method="euclidean")
