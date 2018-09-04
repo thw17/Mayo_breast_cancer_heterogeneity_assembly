5e44a6938059dd5dd3b6e3b3ba76260b02986059
thw17 <twebster17@gmail.com>
2018-09-03 21:18:18 -0700

    add read.table

--- scripts/hclust_to_nexus.R
+++ scripts/hclust_to_nexus.R
@@ -19,8 +19,8 @@ phylo_list <- vector("list", length(input_files))
 
 i <- 1
 while(i < length(input_files)) {
-	print(input_files[i])
-	input_table <- input_files[i][, -1]
+	in1 <- (input_files[i], sep="\t", header=FALSE)
+	input_table <- in1[, -1]
 	rownames(input_table) <- sample_names
 	dist_matrix <- dist(input_table, method="euclidean")
 	fit_hclust <- hclust(dist_matrix, method="ward.D")
